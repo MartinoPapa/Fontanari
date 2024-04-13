5cb73bace95399e7373535ca66290b2750a89b55
MartinoPapa <martino.papa.tn@gmail.com>
2024-04-13 14:58:17 +0200

    form

--- design.js
+++ design.js
@@ -10,4 +10,13 @@ function computeNavbar(){
     var navbar = document.getElementById("navbar");
     var top = (navbar.offsetHeight - homeDiv.offsetHeight) + "px";
     homeDiv.style.top = top;
+    var a = document.getElementById("achi");
+    console.log(a);
+    a.setAttribute('onclick', `window.scrollTo(0, document.getElementById('chi').offsetTop- ${navbar.offsetHeight} );`);
+    a = document.getElementById("acat");
+    a.setAttribute('onclick', `window.scrollTo(0, document.getElementById('catering').offsetTop- ${navbar.offsetHeight} );`);
+    a = document.getElementById("aeve");
+    a.setAttribute('onclick', `window.scrollTo(0, document.getElementById('eventi').offsetTop- ${navbar.offsetHeight-2} );`);
+    a = document.getElementById("acont");
+    a.setAttribute('onclick', `window.scrollTo(0, document.getElementById('contatta').offsetTop- ${navbar.offsetHeight} );`);
 }
