6c74e8f1e6db71112b771d9a118cef43a7f96e09
MartinoPapa <martino.papa.tn@gmail.com>
2024-04-20 17:15:24 +0200

    modifiche

--- design.js
+++ design.js
@@ -11,7 +11,6 @@ function computeNavbar(){
     var top = (navbar.offsetHeight - homeDiv.offsetHeight) + "px";
     homeDiv.style.top = top;
     var a = document.getElementById("achi");
-    console.log(a);
     a.setAttribute('onclick', `window.scrollTo(0, document.getElementById('chi').offsetTop- ${navbar.offsetHeight} );`);
     a = document.getElementById("acat");
     a.setAttribute('onclick', `window.scrollTo(0, document.getElementById('catering').offsetTop- ${navbar.offsetHeight} );`);
@@ -21,4 +20,35 @@ function computeNavbar(){
     a.setAttribute('onclick', `window.scrollTo(0, document.getElementById('contatta').offsetTop- ${navbar.offsetHeight} );`);
     a = document.getElementById("bcont");
     a.setAttribute('onclick', `window.scrollTo(0, document.getElementById('contatta').offsetTop- ${navbar.offsetHeight} );`);
+}
+
+var imgFolder = "img/";
+var vettoreA = ["chi0.jpg", "chi1.jpg", "festa.jpeg"];
+var indexA = 0;
+
+function prevImgA(){
+    imgA = document.getElementById("imgA");
+    --indexA;
+    imgA.src = imgFolder + vettoreA[indexA%vettoreA.length];
+}
+
+function nextImgA(){
+    imgA = document.getElementById("imgA");
+    ++indexA;
+    imgA.src = imgFolder + vettoreA[indexA%vettoreA.length];
+}
+
+var vettoreB = ["chi0.jpg", "chi1.jpg", "festa.jpeg"];
+var indexB = 0;
+
+function prevImgB(){
+    imgB = document.getElementById("imgB");
+    --indexB;
+    imgB.src = imgFolder + vettoreB[Math.abs(indexB%vettoreB.length)];
+}
+
+function nextImgB(){
+    imgB = document.getElementById("imgB");
+    ++indexB;
+    imgB.src = imgFolder + vettoreB[Math.abs(indexB%vettoreB.length)];
 }
